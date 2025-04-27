7ff281c48465880061942eee2735aae5efc9d6f7
Mxiaobo <33818408+Mxiaobo@users.noreply.github.com>
2025-04-27 23:49:34 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='meme-calc-v1';
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['.','index.html','manifest.json','sw.js'])));
+});
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
